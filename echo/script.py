b319a79892efc91e08aed60689f4d76c7cab4872
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2024-06-05 16:37:15 -0500

    Create script.py

---
+++ echo/script.py
@@ -0,0 +1,7 @@
+greeting = "Welcome to my very first chatbot!"
+
+print(greeting)
+print("This chatbot will repeat anything you say.")
+
+stuff_to_echo = input("Say something for the chatbot to repeat!")
+print("You said " + stuff_to_echo)
